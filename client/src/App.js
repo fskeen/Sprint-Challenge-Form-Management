918a38d813293004d78b138a39f084dafc12c4f3
Faye Skeen <fskeen@gmail.com>
2019-08-09 09:45:07 -0700

    App is a class component & updates state without infinite loop.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import './App.css';
 import SignUpForm from './components/SignUpForm'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <SignUpForm />
-    </div>
-  );
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      recipes: ['']
+    };
+  }
+
+  fetchData = () => {
+    axios.get(`http://localhost:5000/api/restricted/data`)
+      .then (res => {
+        console.log(res);
+        this.setState({recipes: res.data});
+      })
+      .catch (err => console.log("Error getting data from server: ", err))
+      
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.recipes.length !== this.state.recipes.length) {
+      this.fetchData()
+    } else {
+      return prevState
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <SignUpForm />
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
